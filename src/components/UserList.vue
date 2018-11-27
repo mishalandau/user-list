87864a154594103302a38e5d95175eaf3622cde0
Misha Landau <landaumichael@yandex.ru>
2018-11-27 17:19:49 +0400

    vue init

---
+++ src/components/UserList.vue
@@ -0,0 +1,24 @@
+<template>
+    <span>{{ userIds }}</span>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+interface IUser {
+    firstName: string;
+    lastName: string;
+    avatar: string;
+    birthday: number;
+}
+
+@Component
+export default class UserList extends Vue {
+    userIds: number[] = [];
+    users: IUser[] = [];
+
+    async created() {
+        this.userIds = await axios.get('http://localhost:3000/user_ids').then(({ data }) => data);
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <UserList/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import UserList from '@/components/UserList.vue';
+
+@Component({
+  components: {
+    UserList,
+  },
+})
+export default class Home extends Vue {}
+</script>
